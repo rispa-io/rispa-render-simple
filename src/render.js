570f4d7a7d2e45dc19111a6bb31df80379d25105
Maxim Samoilov <nitive@icloud.com>
2017-07-14 14:51:29 +0300

    Move template into defaultTemplate file

--- src/render.js
+++ src/render.js
@@ -1,25 +1,11 @@
 import fs from 'fs'
-import { endsWith } from 'ramda'
+import defaultTemplate from './defaultTemplate'
 
 const render = (app, registry) => {
   app.get('/', (req, res) => {
     const stats = res.locals.webpackStats.toJson()
-    const { assets } = stats.entrypoints.main
-    const jsFiles = assets.filter(endsWith('.js'))
-    const cssFiles = assets.filter(endsWith('.css'))
-
-    res.send(`
-      <html>
-        <head>
-          <title>Test</title>
-          ${jsFiles.map(file => `<link rel="stylesheet" href="/${file}">`).join('')}
-        </head>
-        <body>
-          <div id="app">Loading...</div>
-          ${jsFiles.map(file => `<script src="/${file}"></script>`).join('')}
-        </body>
-      </html>
-    `)
+    const template = registry.get('template') || defaultTemplate
+    res.send(template(stats, req, res))
   })
 }
 
