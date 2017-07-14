67df0aaaea8920e51470494bfa2e3930ebaa6a35
Maxim Samoilov <nitive@icloud.com>
2017-07-14 15:13:41 +0300

    Remove console.log

--- .rispa/activator.js
+++ .rispa/activator.js
@@ -3,7 +3,6 @@ import { server } from '@rispa/server/events'
 import render from '../src/render'
 
 const activator = on => {
-  console.log('activator')
   on(init(server), registry => {
     registry.set('render', render)
   })
